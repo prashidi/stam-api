dfbdd4d3f44f3f5bc2ffda566b47a2e6604526e5
Patrick Rashidi <patrickrashid@test4s-MacBook-Air.local>
2019-07-23 13:53:12 +0200

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, \
+    BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -25,11 +26,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """custom user model that supports using email instead of username"""
-    email = models.EmailField(max_length=254, unique=True)
-    name = models.CharField(max_length=254)
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
